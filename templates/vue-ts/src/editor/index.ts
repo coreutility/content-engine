360a64423b64d32bef6cdfe97395f072636331ee
RaghuwanshiAditya <iam.adi.raghu@gmail.com>
2026-01-11 23:59:38 +0530

    Jan, 12, 2025
    Jan, 12, 2025

--- templates/vue-ts/src/editor/index.ts
+++ templates/vue-ts/src/editor/index.ts
@@ -1,4 +1,4 @@
-import type { _p_TYP, _$cb_TYP, _$p_TYP } from "../shared/types";
+import type { _p_TYP, _$p_TYP } from "../shared/types";
 import { helper } from "../shared/util/helper/index";
 import { uuid } from "../shared/util/uuid/index";
 import { hydrator } from "../hydrator";
@@ -7,7 +7,7 @@ const index = async (_p:_p_TYP) => {
 const _helper = await helper();
     
     return {
-        set: async (_$p:_$p_TYP,_$cb?:_$cb_TYP) => {
+        set: async (_$p:_$p_TYP) => {
             const _$r = {
                 r: ``,
                 //style: ``,
@@ -20,12 +20,14 @@ const _helper = await helper();
                 }
             };
             const _renderer = await renderer({
+                my:{},
                 f:{
                     ..._p.f,
                    name:(v:string)=>_helper.f.name({id:curr[`id`],name:v})
                 },
             });
             const _hydrator = await hydrator({
+                my:{},
                 f:{
                     ..._p.f,
                    name:(v:string)=>_helper.f.name({id:curr[`id`],name:v})
@@ -41,31 +43,7 @@ const _helper = await helper();
                 data:{
                     curr:curr,
                 }
-            },{
-                add: (_v) => {
-                    /*console.log(`--add`);
-                    console.log(_v);*/
-                    //set..
-                    let _el = _v.el;
-                    _el?.setAttribute(`contenteditable`,"true"); 
-                    _el?.classList.add('block-content');
-                    _el?.addEventListener("click", () => {
-                       //alert(``);
-                    });
-                    _el?.addEventListener('input', function (event:any) { // input | keyup
-                        const _curr = _el.innerHTML; //_el.textContent;
-                        //update..
-                        _v[`$d`].data = _curr;
-                        _hydrator_rsp.evt.change();
-                    });
-                },
-                change: (_v) => {
-                    /*console.log(`--change`);
-                    console.log(_v);*/
-                    _$cb?.change(_v);
-                }
-
-            });
+            },);
             //set..
             ((_mE)=>{
             const mE = document.createElement("style");
